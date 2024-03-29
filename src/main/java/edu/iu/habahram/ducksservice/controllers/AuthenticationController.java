07271e50e0b337fc159b186a6b38c30589bbe65e
antho_0war6 <aceresol@iu.edu>
2024-03-29 10:26:35 -0400

    new

---
+++ src/main/java/edu/iu/habahram/ducksservice/controllers/AuthenticationController.java
@@ -0,0 +1,25 @@
+package edu.iu.habahram.ducksservice.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import edu.iu.habahram.ducksservice.repository.CustomerRepository;
+import edu.iu.habahram.ducksservice.model.Customer;
+
+@RestController
+@CrossOrigin
+public class AuthenticationController {
+    CustomerRepository customerRepository;
+    public AuthenticationController(CustomerRepository customerRepository){
+        this.customerRepository = customerRepository;
+    }
+    @PostMapping("/signup")
+    public void signup(@RequestBody Customer customer){
+        try{
+            customerRepository.save(customer);
+        }catch(Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+}
